acfc720c391773cbdf56cc0e09f564392da3944e
rajkarthik <121234659+rajkarthik2003@users.noreply.github.com>
2024-02-15 17:20:51 +0530

    Delete learning-portal-complete-main directory

--- learning-portal-complete-main/learning-portal-complete-main/src/main/java/com/karthik/learningportalnew/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.karthik.learningportalnew.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.karthik.learningportalnew.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-}
