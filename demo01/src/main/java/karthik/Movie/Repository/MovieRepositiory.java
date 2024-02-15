56144a04d5a61b162dd003f3242125f494d76641
rajkarthik <121234659+rajkarthik2003@users.noreply.github.com>
2024-02-15 10:53:10 +0530

    Delete demo01 directory

--- demo01/src/main/java/karthik/Movie/Repository/MovieRepositiory.java
+++
@@ -1,8 +0,0 @@
-package karthik.Movie.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import karthik.Movie.Entity.Movie;
-
-public interface MovieRepositiory extends JpaRepository<Movie,Long> {
-}
