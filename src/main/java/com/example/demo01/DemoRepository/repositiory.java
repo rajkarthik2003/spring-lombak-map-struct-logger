df2be92e1b2f04ddb6946ef8a77808d1a98e71a7
rajkarthik <121234659+rajkarthik2003@users.noreply.github.com>
2024-02-15 10:53:32 +0530

    Delete src directory

--- src/main/java/com/example/demo01/DemoRepository/repositiory.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo01.DemoRepository;
-
-import com.example.demo01.Demo.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface repositiory extends JpaRepository<Movie,Long> {
-}
